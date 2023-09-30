d9368882969197020653907f77c13497dea1ff87
diana <diana-safina-2017@mail.ru>
2023-09-30 14:57:26 +0500

    тест исправлен

--- homework-2/tests/test_item.py
+++ homework-2/tests/test_item.py
@@ -2,13 +2,9 @@
 import pytest
 from src.item import Item
 
-
+numb1 = Item("телефон",13000, 10)
 def test_Item_calculate_total_price():
-    numb1 = Item("телефон", 13000, 10)
     assert numb1.calculate_total_price() == 130000
 
-#def test_Item_apply_discount():
-    numb2 = Item("телефон", 13000, 10)
-    Item.pay_rate = 0.5
-    numb2.apply_discount()
-    assert numb2.price == 6500
+def test_Item_apply_discount():
+    assert numb1.apply_discount() == 13000
