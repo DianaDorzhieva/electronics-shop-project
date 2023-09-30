0124e61dc59cf42b43734d3e725e150667e28177
diana <diana-safina-2017@mail.ru>
2023-09-30 14:59:04 +0500

    исправила item

---
+++ homework-1/src/item.py
@@ -0,0 +1,55 @@
+import csv
+
+class Item:
+    """
+    Класс для представления товара в магазине.
+    """
+    pay_rate = 1.0
+    all = []
+
+    def __init__(self, name: str, price: float, quantity: int) -> None:
+        """
+        Создание экземпляра класса item.
+
+        :param name: Название товара.
+        :param price: Цена за единицу товара.
+        :param quantity: Количество товара в магазине.
+        """
+        self.__name = name
+        self.price = price
+        self.quantity = quantity
+        Item.all.append(self)
+
+    @classmethod
+    def instantiate_from_csv(cls, data_word_csv):
+        name, price, quantity = data_word_csv.split(" ")
+        price = float(price)
+        quantity = int(quantity)
+        return cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(some_string):
+        return int(some_string)
+
+    @property
+    def name(self):
+        return self.__name
+
+
+
+    def calculate_total_price(self) -> float:
+        """
+        Рассчитывает общую стоимость конкретного товара в магазине.
+
+        :return: Общая стоимость товара.
+        """
+        return self.price*self.quantity
+
+    def apply_discount(self) -> None:
+        """
+        Применяет установленную скидку для конкретного товара.
+        """
+        self.price *= self.pay_rate
+
+
+
