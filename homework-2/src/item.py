3b1dd69b77a728380b78f00ecbeb0fe35d8f545f
diana <diana-safina-2017@mail.ru>
2023-09-30 16:34:23 +0500

    homework2

--- homework-2/src/item.py
+++ homework-2/src/item.py
@@ -1,3 +1,6 @@
+import csv
+word_csv = []
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +16,33 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        Item.all.append(str(self))
+        Item.all.append(self)
 
+    @classmethod
+    def instantiate_from_csv(cls, file):
+        with open("items.csv") as file:
+            words = csv.DictReader(file)
+            items = []
+            print(words)
+            for word in words:
+                name = word["name"]
+                price = float(word["price"])
+                quantity = int(word["quantity"])
+                item = cls(name, price, quantity)
+                items.append(item)
+            cls.all = items
+        return cls.all
+
+    @staticmethod
+    def string_to_number(some_string):
+        return int(some_string)
+
+    @property
+    def name(self):
+        return self.__name
 
 
     def calculate_total_price(self) -> float:
@@ -32,7 +57,18 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price *= Item.pay_rate
-        return self.price
+        self.price *= self.pay_rate
+
+
+
+with open("items.csv") as file:
+ words = csv.DictReader(file)
+ items = []
+ print(words)
+
+
+
+
+
 
 
