64f44abeb4a3826457bfb596fc6f81b80513e13e
diana <diana-safina-2017@mail.ru>
2023-10-02 13:45:45 +0500

    test

--- homework-2/tests/test_item.py
+++ homework-2/tests/test_item.py
@@ -2,9 +2,23 @@
 import pytest
 from src.item import Item
 
-numb1 = Item("телефон",13000, 10)
+
 def test_Item_calculate_total_price():
+    numb1 = Item("телефон", 13000, 10)
     assert numb1.calculate_total_price() == 130000
 
 def test_Item_apply_discount():
-    assert numb1.apply_discount() == 13000
+    numb1 = Item("телефон", 13000, 10)
+    Item.pay_rate = 0.5
+    assert numb1.price == 6500
+
+def test_Item_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+
+
+def test_Item_len_name():
+    numb1 = Item("телефон", 13000, 10)
+    numb1.name = 'СуперСмартфон'
+    assert numb1.name == 'СуперСмарт'
