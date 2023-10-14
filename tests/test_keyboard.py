19584b3c1615261e99cebe2ba2fe2091dd087471
diana <diana-safina-2017@mail.ru>
2023-10-14 15:58:46 +0500

    test

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -1,11 +1,13 @@
 """Здесь надо написать тесты с использованием pytest для модуля Keyboard."""
 import pytest
-from src.keyboard import Keyboard
+from src.keyboard import Keyboard, Mixinlog
 
 def test_change_lang():
     kb = Keyboard('Dark Project KD87A', 9600, 5)
     assert kb.language == "EN"
     kb.change_lang()
     assert kb.language == "RU"
-    with pytest.raises(ValueError) as e: _= Keyboard('Dark Project KD87A', 9600, 5, "lk")
+    mx = Mixinlog()
+    with pytest.raises(AttributeError) as e:
+                         _= mx.language="rk"
 
