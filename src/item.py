7c3f4cec2e878c1507a40c799a80cf3da8773a46
diana <diana-safina-2017@mail.ru>
2023-10-15 20:35:05 +0500

    src

--- src/item.py
+++ src/item.py
@@ -3,12 +3,11 @@ word_csv = []
 
 
 class InstantiateCSVError(Exception):
-    pass
-    # def __init__(self, *args, **kwargs):
-    #     self.message = args[0] if args else 'Файл item.csv поврежден'
-    #
-    # def __str__(self):
-    #     return self.message
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else 'Файл item.csv поврежден'
+
+    def __str__(self):
+        return self.message
 
 
 
